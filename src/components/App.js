ae134441468438da6f778f3c3114ef8e76466770
David Siddle <siddav83@gmail.com>
2021-10-06 22:51:03 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return(<div>App</div>)
+  }
+}
+
+export default App;
+
+
+//the user can search the book by searching for author, title or espm number imput searchComponent
+//the books will be dislayed in a list - BookList
+//User can select the book - bookiTem
+//User can click on the book and view information on the book and a link to where to purchase / review book
+
